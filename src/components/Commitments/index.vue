e8d568e3785a50908143950cd47b63d4d0999a9a
Rhodes <taylorsingletonfookes@live.com>
2017-02-13 13:38:30 -0800

    commit

--- src/components/Commitments/index.vue
+++ src/components/Commitments/index.vue
@@ -3,8 +3,19 @@
 <div id="commitments">
     <shared-title title='Commitments'>
         <form-box>
-          <label> bitcoin address </label>
-        </form-box>
+            <h6> Create A Commitment Contract: </h6>
+            <label> Commitment Description </label>
+            <input
+                @input="setCommit"
+                type="text"
+                placeholder="What do you commit to do?"
+                name="newcommit"
+                v-model="x"
+            ></input>
+            <label> Send the amount you wish to commit: </label>
+            <img src="../../assets/images/address.svg"></img>
+          </form-box>
+        <h3>Active Commitments:</h3>
         <commitment v-for='c in commitments' :x='c'></commitment>
     </shared-title>
 </div>
@@ -18,6 +29,9 @@ import Commitment from './Commitment'
 import Really from './Really'
 
 export default {
+    data(){
+        return { x:'' }
+    },
     components: {
         SharedTitle,
         FormBox,
@@ -28,6 +42,11 @@ export default {
       commitments(){
         return this.$store.state.commitments.hwaiting
       }
+    },
+    methods: {
+        setCommit(){
+
+        }
     }
 }
 
@@ -35,5 +54,12 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
+
+h3
+    text-align:left
+    font-size:1.9em
+
+input
+    width: 100%
 
 </style>
