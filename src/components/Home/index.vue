9c49f5bb5d6b0f02b92aed95ad4eff8a691f5edb
cammyjee <cameron.john.gray@gmail.com>
2017-04-10 22:20:49 -0700

    home content

--- src/components/Home/index.vue
+++ src/components/Home/index.vue
@@ -1,23 +1,22 @@
 <template lang='jade'>
 
-#projects
-    shared-title(title='DCTRL')
-
-        h3 Links
-        p Check out our&nbsp;
-            a(href='https://www.meetup.com/dctrlvan/') meetup page
-            span .
-        p Have a question or concept you want to discuss, join our&nbsp;
-            a(href='https://decentralslackvite.herokuapp.com/') Slack Chat.
-        p You code? Then find us on&nbsp;
-            a(href='https://github.com/DecentralVan/dctrlapp') github!
-        h3 The App
-        p The&nbsp;
-             a(href='https://github.com/DecentralVan/dctrlapp') dctrlapp
-             span  is community management software for helping communities self-govern themselves in a peer-to-peer way. All the code is open source including a web interface for interaction and information, a&nbsp;
-                a(href='https://github.com/decentralvan/eventstate') backend
-                span &nbsp;for managing members, and a rfid fob system that controls access to commons property.
-
+    #about
+        shared-title(title='About Us')
+            p DCTRL is a Collaborative Community inspired by the ideas behind Bitcoin, Decentralization, and the Open Source movement. This is a meetup where forward thinking people can hangout with like-minded individuals. We believe Cryptocurrency and the Open Source movement can create economically sustainable systems for communities globally and locally. The space is located Downtown Vancouver at &nbsp;
+                a(href='http://maps.google.com/?q=436 w Pender, Vancouver, British Columbia') 436 West Pender Street
+            p Check out our&nbsp;
+                a(href='https://www.meetup.com/dctrlvan/') meetup page
+                span . If you are interested in hosting an event please make a request to info@decentral.network
+            p Have a question or concept you want to discuss? join our&nbsp;
+                a(href='https://decentralslackvite.herokuapp.com/') Slack Chat.
+            p You code? Then find us on&nbsp;
+                a(href='https://github.com/DecentralVan/dctrlapp') github!
+            h3 The App
+            p The&nbsp;
+                 a(href='https://github.com/DecentralVan/dctrlapp') dctrlapp
+                 span  is community management software for helping communities self-govern themselves in a peer-to-peer way. All the code is open source including a web interface for interaction and information, a&nbsp;
+                    a(href='https://github.com/decentralvan/eventstate') backend
+                    span &nbsp;for managing members, and a rfid fob system that controls access to commons property.
 
 
 </template>
