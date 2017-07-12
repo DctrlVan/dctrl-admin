0d91e10895dac5447c6dcb0c32c83fe64d894087
taylor <taylorsingletonfookes@live.com>
2017-07-12 15:56:37 -0700

    Added active bounties sorted by their current value

---
+++ src/components/ActiveBounties/ActiveBs.vue
@@ -0,0 +1,23 @@
+<template lang='jade'>
+
+tr
+    td {{b.name}}
+    td {{b.currentValue}}
+</template>
+
+
+<script>
+
+export default {
+    props: ['b'],
+    components: { },
+
+}
+
+</script>
+
+<style lang="stylus">
+
+// TODO: style .inactive / or .active
+
+</style>
--- src/components/ActiveBounties/index.vue
+++ src/components/ActiveBounties/index.vue
@@ -3,27 +3,59 @@
 #ActiveBounties
     shared-title(title='Active Bounties')
     p Active bounties will be here
+    table.u-full-width
+        thead
+            tr
+                th Bounty Name
+                th Current Value
+        tbody
+            active-bs(v-for="b in bounties", :b="b")
 
 </template>
 
 <script>
-
+import ActiveBs from "./ActiveBs"
+import request from "superagent"
 import SharedTitle from '../slotUtils/SharedTitle'
-// import qrcode from 'qrcode-generator'
+
+function calculatePayout(monthValue, lastClaimed, now){
+    let msSince = now - lastClaimed
+    let today = new Date()
+    let daysThisMonth = new Date(today.getYear(), today.getMonth(), 0).getDate()
+    let msThisMonth = daysThisMonth * 24 * 60 * 60 * 1000
+    return (msSince / msThisMonth) * monthValue
+}
 
 export default {
+  data(){
+      return {
+          bounties: []
+      }
+  },
+  mounted(){
+      request
+          .get('/current_state')
+          .end( (err, res)=> {
+              console.log("response from /current_state::")
+              console.log(res.body)
+              let now = Date.now()
+              let tempBounties = []
+              res.body.bounties.forEach(bounty => {
+                  let processedBounty = {
+                      name: bounty.name,
+                      currentValue: calculatePayout(bounty.value, bounty.notes, now)
+                  }
+                  tempBounties.push(processedBounty)
+              })
+              this.bounties = tempBounties.sort((a,b) => {
+                  return b.currentValue - a.currentValue
+              })
+          })
+  },
     components:{
-        SharedTitle
+        SharedTitle,
+        ActiveBs
     },
-    mounted(){
-      // this does not belong here
-    	// var typeNumber = 4;
-      // var errorCorrectionLevel = 'L';
-      // var qr = qrcode(typeNumber, errorCorrectionLevel);
-      // qr.addData('Hi!');
-      // qr.make();
-      // document.getElementById('test').innerHTML = qr.createImgTag();
-    }
 }
 
 </script>
