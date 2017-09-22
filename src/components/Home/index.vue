6a6944ff5ebfd8c1fb97ab2ba7b6a3b3a9c01702
Taylor Rhodes <taylorsingletonfookes@live.com>
2017-09-22 14:42:00 -0700

    get an address from trezor through browser(hello world trezor)

--- src/components/Home/index.vue
+++ src/components/Home/index.vue
@@ -18,11 +18,30 @@
 <script>
 
 import SharedTitle from '../slotUtils/SharedTitle'
+import TrezorConnect from '../../assets/js/TrezorConnect'
+
+// server-side generated and randomized challenges
+var hosticon = 'https://example.com/icon.png';
+var challenge_hidden = '';
+var challenge_visual = '';
+
+console.log({TrezorConnect})
+var address = "m/44'/0'/2'"
+var coin = "Testnet"
+var segwit = true
+//
+TrezorConnect.getAddress(address, coin, segwit, (response) => {
+  console.log({response}) // window closed instantly
+})
 
 export default {
     components:{
         SharedTitle
     },
+    mounted(){
+
+
+    }
 }
 
 </script>
